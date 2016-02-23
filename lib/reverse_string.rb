4c134100d230c55b4cd8a07cbad158cc5b4f3d10
rynium <ryanstantz@gmail.com>
2016-02-23 01:45:29 +0000

    Done

--- lib/reverse_string.rb
+++ lib/reverse_string.rb
@@ -1,5 +1,25 @@
-def reverse_characters(str)
+def reverse_characters(str)   
+    rev_string = ''
+    split_string = []
+    split_string = str.split("")
+    
+    
+    i = str.length
+    while i > 0 
+      i = i -1
+      rev_string += split_string[i]
+      end
+    return rev_string
 end
 
+
 def reverse_words(str)
+    rev_string = [];
+    split_string = str.split(" ")
+    i = split_string.length
+    while i > 0 
+      i = i -1
+      rev_string.push(split_string[i])
+    end
+    return rev_string.join(" ")
 end
